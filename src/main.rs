cb6fc2bb2f2086614a2abf6ac542cc5ecb774d67
arjav0703 <arjavjain0703@gmail.com>
2025-05-14 11:39:54 +0530

    send 200 OK

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-#[allow(unused_imports)]
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+                // Send a simple HTTP 200 OK response
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                stream.write(response.as_bytes()).unwrap();
+                stream.flush().unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
