a051eb4527de2168b5ecb58ca3dc3b333b51ea05
arjav0703 <arjavjain0703@gmail.com>
2025-05-15 18:04:29 +0530

    add file handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use std::collections::HashMap;
+use std::env;
+use std::fs;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::path::Path;
 use std::thread;
 
 fn main() {
     println!("Logs from your program will appear here!");
+
+    // Parse CLI arguments
+    let args: Vec<String> = env::args().collect();
+    let mut directory = None;
+
+    for i in 0..args.len() - 1 {
+        if args[i] == "--directory" {
+            directory = Some(&args[i + 1]);
+        }
+    }
 
     let listener = TcpListener::bind("0.0.0.0:4221").unwrap();
 
@@ -13,9 +26,10 @@ fn main() {
             Ok(stream) => {
                 println!("Accepted new connection");
 
-                // Spawn a new thread to handle each connection
-                thread::spawn(|| {
-                    handle_req(stream);
+                let dir = directory.map(|s| s.to_string());
+
+                thread::spawn(move || {
+                    handle_req(stream, dir);
                 });
             }
             Err(e) => {
@@ -25,7 +39,7 @@ fn main() {
     }
 }
 
-fn handle_req(mut stream: TcpStream) {
+fn handle_req(mut stream: TcpStream, directory: Option<String>) {
     let reader = BufReader::new(&stream);
     let mut lines = reader.lines();
 
@@ -56,8 +70,10 @@ fn handle_req(mut stream: TcpStream) {
         echo_handler(path)
     } else if path.starts_with("/user-agent") {
         agent_handler(headers)
+    } else if path.starts_with("/files/") && directory.is_some() {
+        file_handler(path, directory.unwrap())
     } else {
-        String::from("HTTP/1.1 404 Not Found\r\n\r\n404 Not Found")
+        String::from("HTTP/1.1 404 Not Found\r\n\r\n")
     };
 
     stream.write(response.as_bytes()).unwrap();
@@ -86,4 +102,25 @@ fn agent_handler(headers: HashMap<String, String>) -> String {
         user_agent.len(),
         user_agent
     )
+}
+
+fn file_handler(path: &str, directory: String) -> String {
+    println!("File request detected: {}", path);
+
+    let filename = path.strip_prefix("/files/").unwrap();
+    println!("Filename: {}", filename);
+
+    let file_path = Path::new(&directory).join(filename);
+    println!("Looking for file at: {:?}", file_path);
+
+    // Check if the file exists and read it
+    if let Ok(contents) = fs::read(&file_path) {
+        format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            String::from_utf8_lossy(&contents)
+        )
+    } else {
+        String::from("HTTP/1.1 404 Not Found\r\n\r\nThe following files are available:")
+    }
 }
